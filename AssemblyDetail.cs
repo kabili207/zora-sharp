9c46028d880fa2fc2e294f7adde006d0c4b0c12c
Andrew Nagle <kabili@zyrenth.com>
2015-04-09 16:39:08 -0400

    Added class to get assembly details.
    Created a shim to make it easier to get details about an assembly. Removed the code that gets the build date because it didn't work under Linux, OSX, or with assemblies embedded inside other assemblies.

---
+++ AssemblyDetail.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Zyrenth.OracleHack
+{
+	/// <summary>
+	/// Provides quick access to details of an assembly
+	/// </summary>
+	public class AssemblyDetail
+	{
+		Assembly _asm;
+
+		/// <summary>
+		/// Gets the assembly company
+		/// </summary>
+		public string Company
+		{
+			get
+			{
+				return GetAttribute<AssemblyCompanyAttribute>(x => x.Company);
+			}
+		}
+
+		/// <summary>
+		/// Gets the assembly copyright information
+		/// </summary>
+		public string Copyright
+		{
+			get
+			{
+				return GetAttribute<AssemblyCopyrightAttribute>(x => x.Copyright);
+			}
+		}
+
+		/// <summary>
+		/// Gets the description about the assembly.
+		/// </summary>
+		public string Description
+		{
+			get
+			{
+				return GetAttribute<AssemblyDescriptionAttribute>(x => x.Description);
+			}
+		}
+
+		/// <summary>
+		/// Gets the assembly's file version
+		/// </summary>
+		public string FileVersion
+		{
+			get
+			{
+				string result = string.Empty;
+				Version version = _asm.GetName().Version;
+				if (version != null)
+					return version.ToString();
+				else
+					return "0.0.0.0";
+			}
+		}
+
+		/// <summary>
+		///  Gets the assembly's full name.
+		/// </summary>
+		public string Product
+		{
+			get
+			{
+				return GetAttribute<AssemblyProductAttribute>(x => x.Product);
+			}
+		}
+
+		/// <summary>
+		/// Gets the assembly's version.
+		/// </summary>
+		public string ProductVersion
+		{
+			get
+			{
+				return GetAttribute<AssemblyInformationalVersionAttribute>(x => x.InformationalVersion) ?? FileVersion;
+			}
+		}
+
+		/// <summary>
+		/// Gets the assembly title
+		/// </summary>
+		public string Title
+		{
+			get
+			{
+				return GetAttribute<AssemblyTitleAttribute>(a => a.Title);
+			}
+		}
+
+		/// <summary>
+		/// Gets the assembly trademark information
+		/// </summary>
+		public string Trademark
+		{
+			get
+			{
+				return GetAttribute<AssemblyTrademarkAttribute>(a => a.Trademark);
+			}
+		}
+
+		/// <summary>
+		/// Creates a new AssemblyDetail object from the specified Assembly
+		/// </summary>
+		/// <param name="asm">The assembly</param>
+		public AssemblyDetail(Assembly asm)
+		{
+			if (asm == null)
+				throw new ArgumentNullException("asm");
+			_asm = asm;
+		}
+
+		private string GetAttribute<T>(Func<T, string> resolveFunc) where T : Attribute
+		{
+			object[] attribs = _asm.GetCustomAttributes(typeof(T), true);
+			if (attribs.Length > 0)
+			{
+				return resolveFunc((T)attribs[0]);
+			}
+			return null;
+		}
+	}
+}
--- Extensions.cs
+++ Extensions.cs
@@ -56,52 +56,6 @@ namespace Zyrenth.OracleHack
 		{
 			return new string(Enumerable.Reverse(value.Substring(start, length)).ToArray());
 		}
-		
-
-		#region Gets the build date and time (by reading the COFF header)
-
-		// http://msdn.microsoft.com/en-us/library/ms680313
-
-		struct _IMAGE_FILE_HEADER
-		{
-			public ushort Machine;
-			public ushort NumberOfSections;
-			public uint TimeDateStamp;
-			public uint PointerToSymbolTable;
-			public uint NumberOfSymbols;
-			public ushort SizeOfOptionalHeader;
-			public ushort Characteristics;
-		};
-
-		public static DateTime GetBuildDateTime(this Assembly assembly)
-		{
-			if (File.Exists(assembly.Location))
-			{
-				var buffer = new byte[Math.Max(Marshal.SizeOf(typeof(_IMAGE_FILE_HEADER)), 4)];
-				using (var fileStream = new FileStream(assembly.Location, FileMode.Open, FileAccess.Read))
-				{
-					fileStream.Position = 0x3C;
-					fileStream.Read(buffer, 0, 4);
-					fileStream.Position = BitConverter.ToUInt32(buffer, 0); // COFF header offset
-					fileStream.Read(buffer, 0, 4); // "PE\0\0"
-					fileStream.Read(buffer, 0, buffer.Length);
-				}
-				var pinnedBuffer = GCHandle.Alloc(buffer, GCHandleType.Pinned);
-				try
-				{
-					var coffHeader = (_IMAGE_FILE_HEADER)Marshal.PtrToStructure(pinnedBuffer.AddrOfPinnedObject(), typeof(_IMAGE_FILE_HEADER));
-
-					return TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1) + new TimeSpan(coffHeader.TimeDateStamp * TimeSpan.TicksPerSecond));
-				}
-				finally
-				{
-					pinnedBuffer.Free();
-				}
-			}
-			return new DateTime();
-		}
-
-		#endregion
 
 	}
 }
