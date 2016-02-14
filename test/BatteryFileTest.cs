07e8204855f371c94e8dba108bc7c38b04730b8a
Andrew Nagle <kabili@zyrenth.com>
2016-02-13 22:36:05 -0500

    Add unit test for battery file loader

---
+++ test/BatteryFileTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace Zyrenth.Zora.Tests
+{
+	[TestFixture]
+	public class BatteryFileTest
+	{
+		[Test]
+		public void TestLoadAll()
+		{
+			Assembly asm = Assembly.GetExecutingAssembly();
+			Stream s = asm.GetManifestResourceStream("Zyrenth.Zora.Tests.AgesSave.sav");
+			IEnumerable<GameInfo> infos = BatteryFileLoader.LoadAll(s);
+			Assert.AreEqual(1, infos.Count());
+			Assert.AreEqual(GameInfoTest.DesiredInfo, infos.First());
+		}
+	}
+}
+
