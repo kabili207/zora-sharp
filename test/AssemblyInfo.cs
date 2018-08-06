04d1959e705b781082d2239d8d104b20c0bbe55b
Henry Gabryjelski <henrygab@users.noreply.github.com>
2018-08-05 23:26:47 -0700

    Fix warning about assembly information for test dll.

---
+++ test/AssemblyInfo.cs
@@ -0,0 +1,52 @@
+/*
+ *  Copyright © 2013-2018, Amy Nagle.
+ *  All rights reserved.
+ *
+ *  This file is part of ZoraSharp.
+ *
+ *  ZoraSharp is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  ZoraSharp is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with ZoraSharp. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("ZoraSharp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZoraSharp.Tests")]
+[assembly: AssemblyCopyright("Copyright © Amy Nagle 2013 - 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("en-US")]
+
+[assembly: CLSCompliant(true)]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("2.2.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
+[assembly: AssemblyInformationalVersion("2.2")]
+
