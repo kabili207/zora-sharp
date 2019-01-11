e3a6dbd428bf480ef9852d32abaefff41c40148f
Amy Nagle <kabi-git@openmuffin.com>
2019-01-11 18:14:19 -0500

    Merge shared logic from US and JP encodings into a base class

--- src/USEncoding.cs
+++ src/USEncoding.cs
@@ -27,7 +27,7 @@ namespace Zyrenth.Zora
 	/// Encodes bytes to characters, or characters to bytes, for US and PAL text.
 	/// Only used for Link and Child names; not for secret encoding.
 	/// </summary>
-	public class USEncoding : Encoding
+	public class USEncoding : BaseEncoding
 	{
 		private readonly char[] characters =
 		{
@@ -45,120 +45,8 @@ namespace Zyrenth.Zora
 		};
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="T:Zyrenth.Zora.USEncoding"/> class.
+		/// Gets the list of characters in this encoding scheme
 		/// </summary>
-		public USEncoding()
-		{
-		}
-
-		/// <summary>
-		/// Calculates the number of bytes produced by encoding a set of characters from the specified character array.
-		/// </summary>
-		/// <returns>The number of bytes produced by encoding the specified characters.</returns>
-		/// <param name="chars">The character array containing the set of characters to encode.</param>
-		/// <param name="index">The index of the first character to encode.</param>
-		/// <param name="count">The number of characters to encode.</param>
-		public override int GetByteCount(char[] chars, int index, int count)
-		{
-			return count;
-		}
-
-		/// <summary>
-		/// Encodes a set of characters from the specified character array into the specified byte array.
-		/// </summary>
-		/// <returns>The actual number of bytes written into bytes.</returns>
-		/// <param name="chars">The character array containing the set of characters to encode.</param>
-		/// <param name="charIndex">The index of the first character to encode.</param>
-		/// <param name="charCount">The number of characters to encode.</param>
-		/// <param name="bytes">The byte array to contain the resulting sequence of bytes.</param>
-		/// <param name="byteIndex">The index at which to start writing the resulting sequence of bytes.</param>
-		public override int GetBytes(char[] chars, int charIndex, int charCount, byte[] bytes, int byteIndex)
-		{
-			for (int i = charIndex; i < charCount; i++)
-			{
-				byte b = 0;
-				char c = chars[i];
-
-				if (c == 0)
-				{
-					b = 0;
-				}
-				else
-				{
-					for (int j = 0; j < characters.Length; j++)
-					{
-						if (characters[j] == c)
-						{
-							b = (byte)( j + 0x10 );
-							break;
-						}
-					}
-				}
-
-				bytes[i] = b;
-			}
-
-			return charCount;
-		}
-
-		/// <summary>
-		/// Calculates the number of characters produced by decoding a sequence of bytes from the specified byte array.
-		/// </summary>
-		/// <returns>The number of characters produced by decoding the specified sequence of bytes.</returns>
-		/// <param name="bytes">The byte array containing the sequence of bytes to decode.</param>
-		/// <param name="index">The index of the first byte to decode.</param>
-		/// <param name="count">The number of bytes to decode.</param>
-		public override int GetCharCount(byte[] bytes, int index, int count)
-		{
-			return count;
-		}
-
-		/// <summary>
-		/// Decodes a sequence of bytes from the specified byte array into the specified character array.
-		/// </summary>
-		/// <returns>The actual number of characters written into chars.</returns>
-		/// <param name="bytes">The byte array containing the sequence of bytes to decode.</param>
-		/// <param name="byteIndex">The index of the first byte to decode.</param>
-		/// <param name="byteCount">The number of bytes to decode.</param>
-		/// <param name="chars">The character array to contain the resulting set of characters.</param>
-		/// <param name="charIndex">The index at which to start writing the resulting set of characters.</param>
-		public override int GetChars(byte[] bytes, int byteIndex, int byteCount, char[] chars, int charIndex)
-		{
-			for (int i = byteIndex; i < byteCount; i++)
-			{
-				int b = bytes[i] - 0x10;
-				if (b < 0 || b >= characters.Length)
-				{
-					chars[i] = '\0';
-				}
-				else
-				{
-					chars[i] = characters[b];
-				}
-			}
-
-			return byteCount;
-		}
-
-		/// <summary>
-		/// Calculates the maximum number of bytes produced by encoding the
-		/// specified number of characters.
-		/// </summary>
-		/// <returns>The maximum number of bytes produced by encoding the specified number of characters.</returns>
-		/// <param name="charCount">The number of characters to encode.</param>
-		public override int GetMaxByteCount(int charCount)
-		{
-			return charCount;
-		}
-
-		/// <summary>
-		/// Calculates the maximum number of characters produced by decoding the specified number of bytes.
-		/// </summary>
-		/// <returns>The number of bytes to decode.</returns>
-		/// <param name="byteCount">The maximum number of characters produced by decoding the specified number of bytes.</param>
-		public override int GetMaxCharCount(int byteCount)
-		{
-			return byteCount;
-		}
+		protected override char[] Characters => characters;
 	}
 }
